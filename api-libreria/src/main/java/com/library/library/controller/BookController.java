3676e2e026a5d6686205c461c23de92929cd852e
Juan López <juanem@hotmail.com>
2022-09-16 19:49:04 -0300

    autenticacion finalizada

--- api-libreria/src/main/java/com/library/library/controller/BookController.java
+++ api-libreria/src/main/java/com/library/library/controller/BookController.java
@@ -12,27 +12,27 @@ import java.util.List;
 public class BookController {
     @Autowired
     private BookService bookService;
-    @GetMapping
+    @GetMapping("/all") // GET http://localhost:8080/books/all (no es necesario logearse)
     public ResponseEntity<List<BookDTO>> getAllBooks(){
         List<BookDTO> books = bookService.getAll();
         return ResponseEntity.ok().body(books);
     }
-    @PostMapping
+    @PostMapping // POST http://localhost:8080/books (necesario logearse)
     public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO dto){
         BookDTO bookSaved = bookService.save(dto);
         return ResponseEntity.ok().body(bookSaved);
     }
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // GET http://localhost:8080/books/{id} (necesario logearse)
     public ResponseEntity<BookDTO> getBookById(@PathVariable Long id){
         BookDTO book = bookService.getBookById(id);
         return ResponseEntity.ok().body(book);
     }
-    @PutMapping("/{id}")
+    @PutMapping("/{id}") // PUT http://localhost:8080/books/{id} (necesario logearse)
     public ResponseEntity<BookDTO> bookUpdate(@PathVariable Long id, @RequestBody BookDTO book) {
         BookDTO bookUpdated = bookService.update(id, book);
         return ResponseEntity.ok().body(bookUpdated);
     }
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}") // DELETE http://localhost:8080/books/{id} (necesario logearse)
     public ResponseEntity<Void> delete(@PathVariable Long id){
         bookService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
