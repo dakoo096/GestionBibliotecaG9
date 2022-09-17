cc53a26241c4c4155f93a71262661d6e34fb20c8
Juan López <juanem@hotmail.com>
2022-09-16 21:04:09 -0300

    bug fixed

--- api-libreria/src/main/java/com/library/library/entity/UserEntity.java
+++ api-libreria/src/main/java/com/library/library/entity/UserEntity.java
@@ -4,8 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 @Entity
 @Table(name = "user")
@@ -13,10 +19,14 @@ import java.util.List;
 @Setter
 @SQLDelete(sql = "UPDATE user SET deleted = true WHERE id = ? ")
 @Where(clause = "deleted = false")
-public class UserEntity{
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+    @Email
+    private String username;
+    @Size(min=8)
+    private String password;
     private String name;
     @Column(name = "last_name")
     private String lastname;
@@ -26,4 +36,40 @@ public class UserEntity{
     private Boolean deleted;
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
     private List<LendingEntity> lendings = new ArrayList<>();
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {this.password = password;}
+    @Override
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
